9bb2f2b6f112f07beb72e2bf6cf586c8a9ca9597
darwinmendozaclaros-dot <darwinmendozaclaros@gmail.com>
2026-03-10 16:32:03 -0300

    Todo Nuevo

---
+++ Darwi.java
@@ -0,0 +1,127 @@
+package darwi;
+
+
+public class Darwi {
+
+  
+    public static void main(String[] args) {
+        int p = 10;
+        int q = 20;
+        System.out.println("p = " + p);
+        System.out.println("q = " + q);
+
+        float x = 5.1f;
+        float y = 2.4f;
+        float z = 7.6f;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+
+        char a = 'A';
+        char b = 'B';
+        char c = 'C';
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+
+        double raiz1 = 3.14;
+        double raiz2 = 6.28;
+        System.out.println("raiz1 = " + raiz1);
+        System.out.println("raiz2 = " + raiz2);
+
+        long contador = 1000;
+        System.out.println("contador = " + contador);
+
+        short indicador = 1;
+        System.out.println("indicador = " + indicador);
+
+        int indice = 5;
+        System.out.println("indice = " + indice);
+
+        double precio = 100.50;
+        double precioFinal = 121.60;
+        System.out.println("precio = " + precio);
+        System.out.println("precioFinal = " + precioFinal);
+
+        char car1 = 'X';
+        char car2 = 'Y';
+        System.out.println("car1 = " + car1);
+        System.out.println("car2 = " + car2);
+
+        byte valor = 50;
+        System.out.println("valor = " + valor);
+
+        boolean primero = true;
+        boolean ultimo = false;
+        System.out.println("primero = " + primero);
+        System.out.println("ultimo = " + ultimo);
+
+        String nombre = "Juan";
+        System.out.println("nombre = " + nombre);
+
+        int edad = 15;
+        System.out.println("edad = " + edad);
+
+        int codigoPostal = 71029;
+        System.out.println("codigoPostal = " + codigoPostal);
+
+        float altura = 4.2f;
+        System.out.println("altura = " + altura);
+
+        char genero = 'H';
+        System.out.println("genero = " + genero);
+
+        String nombre2 = "Daniel";
+        System.out.println("nombre2 = " + nombre2);
+
+        int numeroDeHijos = 2;
+        System.out.println("numeroDeHijos = " + numeroDeHijos);
+
+        double iva = 70.5;
+        System.out.println("iva = " + iva);
+
+        int tallaCamisa = 40;
+        System.out.println("tallaCamisa = " + tallaCamisa);
+
+        float peso = 53.2f;
+        System.out.println("peso = " + peso);
+
+        double precio2 = 9.99;
+        System.out.println("precio2 = " + precio2);
+
+        String alumnoRepetidor = "Si, el alumno repitio";
+        System.out.println("alumnoRepetidor = " + alumnoRepetidor);
+
+        String mensaje = "Hola Mundo";
+        System.out.println("mensaje = " + mensaje);
+
+        char letra = 'D';
+        System.out.println("letra = " + letra);
+
+        boolean mayorEdad = true;
+        boolean menorEdad = false;
+        System.out.println("mayorEdad = " + mayorEdad);
+        System.out.println("menorEdad = " + menorEdad);
+
+        int minuto = 50;
+        System.out.println("minuto = " + minuto);
+
+        int dias = 23;
+        System.out.println("dias = " + dias);
+
+        String matriculaCoche = "AUR 067";
+        System.out.println("matriculaCoche = " + matriculaCoche);
+
+        int contador2 = 42;
+        System.out.println("contador2 = " + contador2);
+
+        boolean mayorDeEdad = false;
+        boolean menorDeEdad = true;
+        System.out.println("mayorDeEdad = " + mayorDeEdad);
+        System.out.println("menorDeEdad = " + menorDeEdad);
+
+        String tallaCamiseta = "XLL";
+        System.out.println("tallaCamiseta = " + tallaCamiseta);
+    }
+    
+}
